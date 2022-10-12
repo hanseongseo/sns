23f6a8a23d023d53260e38880ba21f9a74c9f113
hanseongseo <seeman94@naver.com>
2022-10-12 17:09:22 +0900

    front 코드와 일치시키기 위한 request 객체 필드명 변경

--- src/main/java/personal/sns/controller/request/UserJoinRequest.java
+++ src/main/java/personal/sns/controller/request/UserJoinRequest.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserJoinRequest {
 
-    private String userName;
+    private String name;
     private String password;
 }
--- src/main/java/personal/sns/controller/request/UserLoginRequest.java
+++ src/main/java/personal/sns/controller/request/UserLoginRequest.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserLoginRequest {
 
-    private String userName;
+    private String name;
     private String password;
 }
